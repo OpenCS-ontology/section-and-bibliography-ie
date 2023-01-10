d24fe132d632ff5bd867728405d19d3809cd5994
Paweł Wesołowski <wesolowski.p@protonmail.com>
2023-01-10 21:47:35 +0100

    Lukasz's changes

--- parse_json.py
+++ parse_json.py
@@ -28,6 +28,7 @@ def json_to_rdf(filepath):
     g.add((paper, PO.contains, back_matter := SDP["back-matter"]))
 
     g.add((back_matter, RDF.type, DOCO.BackMatter))
+    g.add((body_matter, RDF.type, DOCO.BodyMatter))
 
     if doc_as_json.get("pdf_parse", {}).get("bib_entries", None):
         bibliography = parse_bibliography(g, doc_as_json)
@@ -35,10 +36,72 @@ def json_to_rdf(filepath):
         g.add((back_matter, CO.firstItem, bibliography_list_item := BNode()))
         g.add((bibliography_list_item, CO.itemContent, bibliography))
 
+    if doc_as_json.get("pdf_parse", {}).get("body_text", None):
+        parse_sections(g, doc_as_json, body_matter)
+
     output_name = re.sub(r"\s+", "", doc_as_json.get("title", "")) + "_sections_biblio_ie"
     output_path = Path(filepath.parent) / f"{output_name}.ttl"
     g.serialize(destination=output_path, format="turtle")
     output_path.chmod(0o666)
+
+def parse_sections(g, doc_as_json, body_matter):
+    sections_dict = {}
+    first_iter = True
+    first_iter_inner = True
+    hierarchy = ["", ""]
+    for i, paragraph in enumerate(doc_as_json.get("pdf_parse", {}).get("body_text", [])):
+        section_name: str = paragraph["section"]
+        section_number: str = paragraph["sec_num"]
+        if (section_name, section_number) not in sections_dict:
+            g.add((section:= SDP[f"section{section_name.title().replace(' ', '').replace('.', '')}"], RDF.type, DOCO.Section))
+            g.add((section_title:=SDP[f"sectionTitle{section_name.title().replace(' ', '').replace('.', '')}"], RDF.type, DOCO.SectionTitle))
+            g.add((section_title, C4O.hasContent, Literal(section_name)))
+            g.add((section, PO.containsAsHeader, section_title))
+            sections_dict[(section_name, section_number)] = section
+
+
+            depth = len(str(section_number).split("."))
+            print(section_name, section_number, depth)
+            if depth == 1:
+                if hierarchy[0] == "":
+                    g.add((body_matter, CO.firstItem, body_list_item := BNode()))
+                else:
+                    g.add((body_list_item, CO.nextItem, next_body_list_item := BNode()))
+                    body_list_item = next_body_list_item
+                g.add((body_list_item, CO.itemContent, section))
+                g.add((body_matter, PO.contains, section))
+                hierarchy[0] = section
+                first_iter = True
+            elif depth == 2:
+                if first_iter:
+                    g.add((hierarchy[0], CO.firstItem, list_item := BNode()))
+                else:
+                    g.add((list_item, CO.nextItem, next_list_item := BNode()))
+                    list_item = next_list_item
+                g.add((list_item, CO.itemContent, section))
+                g.add((hierarchy[0], PO.contains, section))
+                hierarchy[1] = section
+                first_iter = False
+                first_iter_inner = True
+            else:
+                if first_iter_inner:
+                    g.add((hierarchy[1], CO.firstItem, list_item_inner := BNode()))
+                else:
+                    g.add((list_item_inner, CO.nextItem, next_list_item_inner := BNode()))
+                    list_item_inner = next_list_item_inner
+                g.add((list_item_inner, CO.itemContent, section))
+                g.add((hierarchy[1], PO.contains, section))
+                first_iter_inner = False
+        else:
+            section = sections_dict[(section_name, section_number)]
+
+        citations = paragraph['cite_spans']
+        if citations:
+            for citation in citations:
+                g.add((section, PO.contains, citation_node := SDP[f"referenceTo{citation['ref_id']}"]))
+                g.add((citation_node, RDF.type, DEO.Reference))
+                g.add((citation_node, C4O.hasContent, Literal(citation['text'])))
+                g.add((citation_node, DCTERMS.references, SDP[citation['ref_id']]))
 
 
 def parse_bibliography(g, doc_as_json):
