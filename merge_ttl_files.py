8addc3aa0c7e054c21070b73ef6f8c04822ec0df
grzegorzZ1 <lukgrzes@gazeta.pl>
2023-11-10 19:41:38 +0100

    Bug fix

--- merge_ttl_files.py
+++ merge_ttl_files.py
@@ -7,52 +7,47 @@ def merge_ttl(ttl_file_org, ttl_file_add):
     g1 = Graph()
     g2 = Graph()
 
-    print(f"org :{ttl_file_org}")
-    print(f"add :{ttl_file_add}")
-
     g1.parse(ttl_file_org, format="ttl")
     g2.parse(ttl_file_add, format="ttl")
-    
+
     fabio = Namespace("http://purl.org/spar/fabio/")
     bn = Namespace("https://w3id.org/ocs/ont/papers/")
     g1.bind("fabio", fabio)
     g1.bind("bn", bn)
     g2.bind("fabio", fabio)
     g2.bind("", bn)
-    
-    
+
     paper_org = g1.value(predicate=RDF.type, object=fabio.ResearchPaper)
-    paper_add = URIRef(bn+"paper")
-    
-    id_ = re.search(r'[a-zA-Z0-9]{9}$', str(paper_org)).group(0)
-    
-    for s,p,o in g2.triples((None, None, None)):
+    paper_add = URIRef(bn + "paper")
+
+    id_ = re.search(r"[a-zA-Z0-9]{9}$", str(paper_org)).group(0)
+
+    for s, p, o in g2.triples((None, None, None)):
         new_s = s
         new_o = o
-        
+
         if s == paper_add:
             new_s = paper_org
-            
+
         elif not isinstance(s, BNode):
             sub = str(s)
             sub = sub + "_" + id_
-            new_s = (URIRef(sub))
-                    
-    
+            new_s = URIRef(sub)
+
         if not isinstance(o, (Literal, BNode)):
             obj = str(o)
             obj = obj + "_" + id_
-            new_o = (URIRef(obj))
-            
+            new_o = URIRef(obj)
+
         if p == RDF.type:
             new_o = o
-        
-        g2.remove((s,p,o))
+
+        g2.remove((s, p, o))
         g2.add((new_s, p, new_o))
-            
+
     for prefix, namespace in g2.namespaces():
         g1.namespace_manager.bind(prefix, namespace)
-    
+
     g1 += g2
 
     return g1.serialize(format="turtle")
@@ -74,15 +69,19 @@ if __name__ == "__main__":
                         if ttl_file.lower() == out_ttl.lower():
                             final_out_path = out_ttl
 
-                    g = merge_ttl(
-                        ttl_file_org=os.path.join(dir_path, ttl_file),
-                        ttl_file_add=os.path.join(
-                            output_path, archive, dir, final_out_path
-                        ),
-                    )
-                    with open(os.path.join(dir_path, ttl_file), "wb") as file:
-                        if isinstance(g, str):
-                            g = g.encode()
-                        file.write(g)
-                    print(f"File {ttl_file} merged")
-
+                    try:
+                        g = merge_ttl(
+                            ttl_file_org=os.path.join(dir_path, ttl_file),
+                            ttl_file_add=os.path.join(
+                                output_path, archive, dir, final_out_path
+                            ),
+                        )
+                        with open(os.path.join(dir_path, ttl_file), "wb") as file:
+                            if isinstance(g, str):
+                                g = g.encode()
+                            file.write(g)
+                        print(f"File {ttl_file} merged")
+                    except:
+                        print(
+                            f"Cannot scraper more information from {ttl_file} file, final ttl will be creating based on input file"
+                        )
